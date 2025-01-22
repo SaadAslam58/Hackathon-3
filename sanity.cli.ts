96325b4087284a6fd259fe7fac1dc21230bb0615
SaadAslam58 <saadxpert58@gmail.com>
2025-01-22 08:06:19 +0500

    fourth commit

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -6,6 +6,5 @@ import { defineCliConfig } from 'sanity/cli'
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
-const write = process.env.SANITY_WRITE_TOKEN
 
 export default defineCliConfig({ api: { projectId, dataset } })
