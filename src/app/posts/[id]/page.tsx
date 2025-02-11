39e0cc467fd0aa9dc362df4ba9e1b644d49008e7
SaadAslam58 <saadxpert58@gmail.com>
2025-02-11 07:13:50 +0500

    fourth commit

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -167,7 +167,14 @@ const ProductPage: React.FC = () => {
     router.push("/Checkout");
   };
 
-  if (!product) return <div>Loading...</div>;
+  if (!product) {
+    return (
+      <div className="w-full min-h-screen flex items-center justify-center">
+        <div className="w-12 h-12 border-4 border-gray-300 border-t-[black] rounded-full animate-spin"></div>
+      </div>
+    );
+  }
+  
   return (
     <div className="w-full min-h-screen flex flex-col items-center bg-gray-100">
       {/* Product Details */}
